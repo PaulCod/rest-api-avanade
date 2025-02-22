31e0c0728418c0b89c0f1d1571393b1147e0eec9
Paulo <paulolds086543@gmail.com>
2025-02-22 01:50:46 -0300

    Implementação da camada de dominio

---
+++ src/main/java/dio/rest_api_avanade/domain/model/Feature.java
@@ -0,0 +1,12 @@
+package dio.rest_api_avanade.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_features")
+public class Feature extends BaseItem {
+
+
+}
---
+++ src/main/java/dio/rest_api_avanade/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package dio.rest_api_avanade.domain.repository;
+
+import dio.rest_api_avanade.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
