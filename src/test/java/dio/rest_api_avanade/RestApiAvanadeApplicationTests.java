2bb65b21834eed100926622b597b39b978b34877
Paulo <paulolds086543@gmail.com>
2025-02-22 00:46:54 -0300

    Initial commit

---
+++ src/test/java/dio/rest_api_avanade/RestApiAvanadeApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.rest_api_avanade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiAvanadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
